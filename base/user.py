9d9d11153d353b566cd0aec2315db29f4be412bd
Аким Малыщик <akimich11@yandex.by>
2021-09-19 20:27:52 +0300

    fixed user init bug

--- base/user.py
+++ base/user.py
@@ -1,6 +1,6 @@
 class User:
     def __init__(self, user_id, username, first_name, last_name,
-                 department, is_admin, is_voenka, former_group, skips_month, skips_semester):
+                 department, is_admin, is_voenka, former_group, skips_month, skips_semester, *args):
         self.username = None if username == 'undefined' else username
         self.first_name = first_name
         self.last_name = last_name
