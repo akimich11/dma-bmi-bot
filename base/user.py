2a082a21aefa356b23466dbfe480917f44746ca2
Akim Malyschik <akimmalyschik@coherentsolutions.com>
2021-09-22 08:57:32 +0300

    Removed properties

--- base/user.py
+++ base/user.py
@@ -1,31 +1,9 @@
 class User:
     def __init__(self, **kwargs):
-        self.raw_data = kwargs
-
-    @property
-    def username(self):
-        return None if self.raw_data['username'] == 'undefined' else self.raw_data['username']
-
-    @property
-    def first_name(self):
-        return self.raw_data['first_name']
-
-    @property
-    def last_name(self):
-        return self.raw_data['last_name']
-
-    @property
-    def id(self):
-        return int(self.raw_data['id'])
-
-    @property
-    def is_admin(self):
-        return bool(self.raw_data['is_admin'])
-
-    @property
-    def skips_month(self):
-        return self.raw_data['skips_month']
-
-    @property
-    def skips_semester(self):
-        return self.raw_data['skips_semester']
+        self.username = None if kwargs['username'] == 'undefined' else kwargs['username']
+        self.first_name = kwargs['first_name']
+        self.last_name = kwargs['last_name']
+        self.id = int(kwargs['id'])
+        self.is_admin = bool(kwargs['is_admin'])
+        self.skips_month = kwargs['skips_month']
+        self.skips_semester = kwargs['skips_semester']
