d0d1f680c62f39559f2c76bc4ef8a68994d8f661
Akim Malyschik <akimich11@yandex.by>
2022-01-06 16:35:35 +0300

    fixed import bug

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
-from threading import Thread
+# from threading import Thread
 
 import config
-import schedule
+# import schedule
 from base.bot import bot
-from schedules.utils import send_periodic_question, schedule_check, get_questions
+# from schedules.utils import send_periodic_question, schedule_check, get_questions
 import skips.views
 import polls.views
 import users.views
@@ -11,12 +11,12 @@ import users.views
 
 
 if __name__ == '__main__':
-    questions = get_questions()
-    for question in questions:
-        getattr(schedule.every(), question.weekday
-                ).at(question.time).do(send_periodic_question, question=question)
-
-    Thread(target=schedule_check).start()
+    # questions = get_questions()
+    # for question in questions:
+    #     getattr(schedule.every(), question.weekday
+    #             ).at(question.time).do(send_periodic_question, question=question)
+    #
+    # Thread(target=schedule_check).start()
     bot.send_message(config.AKIM_ID, 'started')
 
 bot.polling(none_stop=True)
