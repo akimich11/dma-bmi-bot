409257357f034efbace555c502f116d3e084c7ef
Akim Malyschik <akimich11@yandex.by>
2022-02-02 19:27:49 +0300

    removed unused statement with None

--- users/service.py
+++ users/service.py
@@ -1,24 +1,22 @@
 from base.decorators import db
 
-# todo: проверки на деда
-
 
 class UserService:
     @staticmethod
     @db.fetch(return_type='value')
-    def get_group_chat_id(user_id, cursor=None):
+    def get_group_chat_id(user_id, cursor):
         cursor.execute("SELECT departments.chat_id FROM users "
                        "JOIN departments ON users.department_id = departments.id "
                        "WHERE users.id=(%s)", (user_id,))
 
     @staticmethod
     @db.fetch(return_type='tuple')
-    def get_name(user_id, cursor=None):
+    def get_name(user_id, cursor):
         cursor.execute("SELECT first_name, last_name FROM users WHERE id=(%s)", (user_id,))
 
     @staticmethod
     @db.fetch(return_type='value')
-    def get_is_admin(user_id, cursor=None):
+    def get_is_admin(user_id, cursor):
         cursor.execute("SELECT is_admin FROM users WHERE id=(%s)", (user_id,))
 
     @staticmethod
